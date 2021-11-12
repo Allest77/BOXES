18b2289b26e81ff52078f14a3e413ee59ca4f14e
Allest77 <andr0idph0ne6969@gmail.com>
2021-11-11 20:45:26 -0500

    Readded driving
    I accidentally made it in prototype 1 and not the final game project

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class FollowPlayer : MonoBehaviour {
     public GameObject player;
-    private Vector3 offset = new Vector3(0, 5, -11.76f);
+    private Vector3 offset = new Vector3(0, 2, -11.50f);
 
     void LateUpdate() {
         transform.position = player.transform.position + offset;
