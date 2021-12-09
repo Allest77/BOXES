df2930a963c3749f0b6925dcd670a01b5c235f80
Allest77 <30967524+Allest77@users.noreply.github.com>
2021-12-09 12:29:00 -0500

    FIXED SPEED REDUCTION?

--- Assets/Scripts/Packages/SpeedReduction.cs
+++ Assets/Scripts/Packages/SpeedReduction.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class SpeedReduction : MonoBehaviour {
     public GameObject[] packagePrefabs; //create a package array
     private ThirdPersonMovement player; //Reference Third Person Controller Script
+    float minAmount, envelopeAmount, plasticAmount, sBoxAmount, mBoxAmount, lBoxAmount, customAmount;
 
     void Start() {
         player = GameObject.Find("ThirdPersonPlayer").GetComponent<ThirdPersonMovement>(); //reference player speed, taken from this script.
@@ -12,10 +13,16 @@ public class SpeedReduction : MonoBehaviour {
 
     void SpeedReduction() {
         //Speed Reduction Variables (slows down the player's speed. Subtract player's current speed by amount variables);
-        public float minAmount = Random.Range(1.1f, 3.0f), envelopeAmount = Random.Range(3.4f, 6.0f), plasticAmount = Random.Range(4.6f, 6.3f), sBoxAmount = Random.Range(2.4f, 12.6f), mBoxAmount = Random.Range(8.0f, 16.8f), lBoxAmount = Random.Range(12.0f, 26.9f), maxAmount = Random.Range(29.0f, 88.1f);
+        minAmount = Random.Range(1.1f, 3.0f); 
+        envelopeAmount = Random.Range(3.4f, 6.0f); 
+        plasticAmount = Random.Range(4.6f, 6.3f); 
+        sBoxAmount = Random.Range(2.4f, 12.6f); 
+        mBoxAmount = Random.Range(8.0f, 16.8f); 
+        lBoxAmount = Random.Range(12.0f, 26.9f); 
+        maxAmount = Random.Range(29.0f, 88.1f);   
         
         //The customAmount is determined by a random range of weights
-        public float customAmount = Random.Range(packagePrefabs[minAmount], packagePrefabs[envelopeAmount], packagePrefabs[plasticAmount], packagePrefabs[sBoxAmount], packagePrefabs[mBoxAmount], packagePrefabs[lBoxAmount], packagePrefabs[maxAmount]);
+        customAmount = Random.Range(packagePrefabs[minAmount], packagePrefabs[envelopeAmount], packagePrefabs[plasticAmount], packagePrefabs[sBoxAmount], packagePrefabs[mBoxAmount], packagePrefabs[lBoxAmount], packagePrefabs[maxAmount]);
     } 
 
 
