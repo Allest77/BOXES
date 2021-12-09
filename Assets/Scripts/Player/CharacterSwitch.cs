03c136a8c46684a1ecb575a4459ef423ca26f710
Allest77 <30967524+Allest77@users.noreply.github.com>
2021-12-09 12:18:47 -0500

    FIX SOMETHING
    SpeedReduction.cs is almost completed, needs to be fixed
    Point of Delivery system almost done

--- Assets/Scripts/Player/CharacterSwitch.cs
+++ Assets/Scripts/Player/CharacterSwitch.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CharacterSwitch : MonoBehaviour {
-    /*public ThirdPersonMovement player;
+    public GameObject[] playerPrefabs; //create a player array
 
-    public void Awake()
-    {
-     
+    public void Awake() {
+        vroom.SetActive(true);
+        player.SetActive(false);
+    }
+
+    void Switch() {
+        int playerIndex = (0, packagePrefabs.Length);
     }
 
     // Update is called once per frame
@@ -17,5 +21,5 @@ public class CharacterSwitch : MonoBehaviour {
             vroom.SetActive(false);
             player.SetActive(true);
         }
-    } */
+    }
 }
