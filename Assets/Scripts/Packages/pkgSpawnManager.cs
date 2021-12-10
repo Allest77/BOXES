d1fa089d29d5d03ad697a27b1d5a7a34f93f6ef3
Allest77 <andr0idph0ne6969@gmail.com>
2021-12-10 17:33:35 -0500

    Spawn Manager

--- Assets/Scripts/Packages/pkgSpawnManager.cs
+++ Assets/Scripts/Packages/pkgSpawnManager.cs
@@ -12,20 +12,18 @@ public class pkgSpawnManager : MonoBehaviour {
     
     public List<int> stops = new List<int>();
 
-    //stops.Add() - For loop
-
     void Start() {
         SpawnRandomPackage();
 
         for (int i = 0; i < Random.Range(minStops, maxStops); i++) {
-            stops.Add(60);
+            stops.Add(Random.Range(1, 199)); //NEXT STOP INSTEAD OF RANDOM STOPS, start from 1 - # (between 1 & 200).
         }
     }
                                         
     //Multiple Points of Delivery. Locations prefab will hold a random # of packages per POD in a stop. There can sometimes be more than one POD
 
     void SpawnRandomPackage() {
-        //int locationIndex = Random.Range(0, locationPrefab.Length); //Spawn random # of PODs (should be per stop).
+        int locationIndex = Random.Range(0, locationsPrefabs.Length); //Spawn random # of PODs (should be per stop).
         routeIndex = Random.Range(0, uiSprites.Length); //Randomly spawns a package (your current delivery).
         uiImage.sprite = uiSprites[routeIndex];
     }
